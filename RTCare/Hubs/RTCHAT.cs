c98ae15ffec320a1b02e51934e3429aa8fcd88e2
daffasyaidina <daffasyaidina89@gmail.com>
2022-01-05 16:03:51 +0700

    Uploaded files

---
+++ RTCare/Hubs/RTCHAT.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace RTCare.Hubs
+{
+    public class RTCHAT : Hub
+    {
+        public async Task SendMessage(string username, string message) 
+        {
+            await Clients.All.SendAsync("RecieveMessage", username, message);
+        }
+
+    }
+}
